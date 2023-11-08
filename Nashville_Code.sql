0558ec4080de1d4a05f59ad3df1759fdf5aac50e
Daniel Ogboi <103626807+Presidofield1@users.noreply.github.com>
2023-11-08 00:45:11 +0000

    Update Nashville_Code.sql

--- Nashville_Code.sql
+++ Nashville_Code.sql
@@ -22,9 +22,9 @@ RENAME COLUMN ï»¿UniqueID TO UniqueID;
 -- Firstly we convert to date format in form of '%Y-%m-%d'
 UPDATE Nash
 SET SaleDate = CASE 
-					WHEN SaleDate Like '% , %' THEN date_format(str_to_date(SaleDate, '%M %d, %Y'), '%Y-%m-%d')
-                    ELSE date_format(str_to_date(SaleDate, '%M %d, %Y'), '%Y-%m-%d')
-				END;
+		  WHEN SaleDate Like '% , %' THEN date_format(str_to_date(SaleDate, '%M %d, %Y'), '%Y-%m-%d')
+                  ELSE date_format(str_to_date(SaleDate, '%M %d, %Y'), '%Y-%m-%d')
+		END;
 
 -- Then set data type to date
 ALTER TABLE Nash
@@ -38,23 +38,23 @@ WHERE PropertyAddress LIKE '' OR OwnerName LIKE '';
 
 UPDATE Nash
 SET PropertyAddress = CASE 
-						WHEN PropertyAddress LIKE '' THEN null 
+		        WHEN PropertyAddress LIKE '' THEN null 
                         ELSE PropertyAddress
-					  END,
-    OwnerName = CASE 
-					WHEN OwnerName LIKE '' THEN null
-					ELSE OwnerName
-				END;
+		      END,
+	 OwnerName = CASE 
+			WHEN OwnerName LIKE '' THEN null
+			ELSE OwnerName
+		      END;
 
 
 SELECT DISTINCT SoldAsVacant FROM Nash;
 -- There are Two outcome, so we could just make everything uniform by either Yes or No
 UPDATE Nash
 SET SoldAsVacant = CASE
-						WHEN SoldAsVacant LIKE 'N%' THEN 'No'
+			WHEN SoldAsVacant LIKE 'N%' THEN 'No'
                         WHEN SoldAsVacant LIKE 'Y%' THEN 'Yes'
                         ELSE null
-					END;
+		    END;
                     
 -- Let's remove duplicates and fill the NULL values
 SELECT a.ParcelID, a.PropertyAddress, b.PropertyAddress, a.OwnerName, b.OwnerName FROM Nash a
@@ -80,16 +80,17 @@ HAVING COUNT(*) > 1 ;
 
 WITH DuplCTE AS (
 	Select *, 
-			ROW_NUMBER() OVER(
-								PARTITION BY ParcelID, PropertyAddress, OwnerName, SalePrice
-                                ORDER BY UniqueID) Duplicated
-		FROM Nash)
+		ROW_NUMBER() OVER(PARTITION BY ParcelID, PropertyAddress, OwnerName, SalePrice
+                ORDER BY UniqueID) Duplicated
+	FROM Nash)
+	
 DELETE FROM DuplCTE
 WHERE Duplicated >1;
 
 DELETE a FROM Nash a
 	INNER JOIN Nash b
-		WHERE a.PropertyAddress < b.PropertyAddress AND a.ParcelID = b.ParcelID;
+	WHERE a.PropertyAddress < b.PropertyAddress 
+		AND a.ParcelID = b.ParcelID;
     
 
                     
@@ -105,13 +106,13 @@ HAVING COUNT(*) >1;
 
 ALTER TABLE Nash
 ADD COLUMN ( City VARCHAR (50),
-			State VARCHAR (30));
+State VARCHAR (30));
  
  -- Insert values into created tables
 UPDATE Nash
 SET City = SUBSTRING_INDEX(SUBSTRING_INDEX(OwnerAddress, ', ', -2), ',', 1),
-	State = SUBSTRING_INDEX(OwnerAddress, ',', -1),
-	PropertyAddress = SUBSTRING_INDEX(PropertyAddress, ',', 1);
+State = SUBSTRING_INDEX(OwnerAddress, ',', -1),
+PropertyAddress = SUBSTRING_INDEX(PropertyAddress, ',', 1);
 
 -- Removing Unnecessary Columns 
 ALTER TABLE Nash
